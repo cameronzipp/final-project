c6344a7b9b2fa7604a8af30926986026770c8aad
Cameron <camerontzipp@comcast.net>
2021-05-04 15:00:18 -0400

    Added index page

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//this is the controller class
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require autoload files
+require_once ('vendor/autoload.php');
+
+//instantiate fat-free
+$f3 = Base::instance();
+
+//define routes
+$f3->route('GET /', FUNCTION(){
+    //display the home page
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//run fat-free
+$f3->run();
