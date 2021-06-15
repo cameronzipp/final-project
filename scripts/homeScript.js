5d0ac93ba3f3e1d39b7fcbba15d34305fa02bc92
Cameron <camerontzipp@comcast.net>
2021-06-15 16:32:51 -0400

    Added linking for forgotten login credentials and added additional comments to script

--- scripts/homeScript.js
+++ scripts/homeScript.js
@@ -3,6 +3,7 @@ document.getElementById("homeLoginForm").onsubmit = validate;
 function validate() {
     let isValid = true;
 
+    //set all errors to be hidden
     let errors = document.getElementsByClassName("err");
     for (let i = 0; i < errors.length; i++){
         errors[i].style.display = "none";
@@ -10,7 +11,7 @@ function validate() {
 
     // Check username - Must not be empty, or 4 or less characters
     let username = document.getElementById("uname").value;
-    if (!username.match("/(?=.*[a-z]).{4,}/")) { //username === ""
+    if (!username.match("/(?=.*[a-z]).{4,}/")) {
         let errUser = document.getElementById("err-user");
         errUser.style.display = "inline";
         isValid = false;
@@ -18,7 +19,7 @@ function validate() {
 
     //Check password - Must not be empty, 8 or less characters, and must include a special character
     let password = document.getElementById("pass").value;
-    if (!password.match("/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/")) { //password === ""
+    if (!password.match("/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/")) { //number, letter, capital letter, 8 chars
         let errPass = document.getElementById("err-pass");
         errPass.style.display = "inline";
 
@@ -29,7 +30,7 @@ function validate() {
 }
 
 document.getElementById("pass").onclick = logPassVisible;
-
+//make the password on the login (home) page toggleable
 function logPassVisible() {
     let logPass = document.getElementById("pass");
     if (logPass.type === "password") {
@@ -40,7 +41,7 @@ function logPassVisible() {
 }
 
 document.getElementById("homeLoginForm").onsubmit = validateLog;
-
+//make the password on the register page toggleable
 function validateLog() {
     let isValid = true;
 
@@ -67,14 +68,3 @@ function validateLog() {
 
     return isValid;
 }
-
-// document.getElementById("pass").onclick = logPassVisible;
-//
-// function logPassVisible() {
-//     let logPass = document.getElementById("pass");
-//     if (logPass.type === "password") {
-//         logPass.type = "text";
-//     } else {
-//         logPass.type = "password";
-//     }
-// }
