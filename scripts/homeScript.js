be3f295a4d0e83c4ca71b8f6cde4c3f093c5085f
Cameron <camerontzipp@comcast.net>
2021-06-15 03:11:21 -0400

    catching up with main

--- scripts/homeScript.js
+++ scripts/homeScript.js
@@ -9,16 +9,16 @@ function validate() {
     }
 
     // Check username - Must not be empty, or 4 or less characters
-    let username = document.getElementById("uname").value;
-    if (username = "" /*!"/(?=.*[a-z]).{4,}/"*/) {
+    // let username = document.getElementById("uname").value;
+    if (!"/(?=.*[a-z]).{4,}/") { //username === ""
         let errUser = document.getElementById("err-user");
         errUser.style.display = "inline";
         isValid = false;
     }
 
     //Check password - Must not be empty, 8 or less characters, and must include a special character
-    let password = document.getElementById("pass").value;
-    if (password = "" /*!"/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/"*/){
+    // let password = document.getElementById("pass").value;
+    if (!"/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/"){ //password === ""
         let errPass = document.getElementById("err-pass");
         errPass.style.display = "inline";
 
