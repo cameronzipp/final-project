8a467a5e8247482f3ed65a252747ac0a6a200d38
Hunter Ahlquist <36090375+HunterAhlquist@users.noreply.github.com>
2021-06-15 00:32:42 -0700

    fixed client validation on login page

--- scripts/homeScript.js
+++ scripts/homeScript.js
@@ -1,6 +1,6 @@
-document.getElementById("homeLoginForm").onsubmit = validate;
+document.getElementById("homeLoginForm").onsubmit = validateLog;
 
-function validate() {
+function validateLog() {
     let isValid = true;
 
     let errors = document.getElementsByClassName("err");
@@ -9,32 +9,31 @@ function validate() {
     }
 
     // Check username - Must not be empty, or 4 or less characters
-    // let username = document.getElementById("uname").value;
-    if (!"/(?=.*[a-z]).{4,}/") { //username === ""
+    let username = document.getElementById("uname").value;
+    if (username.length <= 0) { //username === ""
         let errUser = document.getElementById("err-user");
         errUser.style.display = "inline";
         isValid = false;
     }
 
     //Check password - Must not be empty, 8 or less characters, and must include a special character
-    // let password = document.getElementById("pass").value;
-    if (!"/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/"){ //password === ""
+    let password = document.getElementById("pass").value;
+    if (password.length <= 0){ //password === ""
         let errPass = document.getElementById("err-pass");
         errPass.style.display = "inline";
-
         isValid = false;
     }
 
     return isValid;
 }
 
-document.getElementById("pass").onclick = logPassVisible;
-
-function logPassVisible() {
-    let logPass = document.getElementById("pass");
-    if (logPass.type === "password") {
-        logPass.type = "text";
-    } else {
-        logPass.type = "password";
-    }
-}
+// document.getElementById("pass").onclick = logPassVisible;
+//
+// function logPassVisible() {
+//     let logPass = document.getElementById("pass");
+//     if (logPass.type === "password") {
+//         logPass.type = "text";
+//     } else {
+//         logPass.type = "password";
+//     }
+// }
